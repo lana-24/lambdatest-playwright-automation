814ac2b0aa55291a896912b10f10d4c55680731b
olansolve <maulmalikib@gmail.com>
2026-05-16 14:26:00 +0700

    feat: add function click checkoit

--- src/pages/header.py
+++ src/pages/header.py
@@ -11,30 +11,41 @@ class HeaderComponent:
         self.page.get_by_role("link", name=" Home").click()
         return HomePage(self.page)
 
-    def go_to_cart(self):
-        from src.pages.cart_page import CartPage
+    def click_cart(self):        
         logger.info("click Cart")
         self.page.locator(".cart").click()
+
+    def click_edit_cart(self):
+        from src.pages.cart_page import CartPage
+        logger.info("click edit cart")
+        self.page.get_by_role("button",name=" Edit cart").click()
         return CartPage(self.page)
+    
+    def click_checkout(self):
+        from src.pages.checkout_page import CheckoutPage
+        logger.info("click checkout")
+        self.page.get_by_role("button", name=" Checkout").click()
+        logger.debug("switch to ChekcoutPage")
+        return CheckoutPage(self.page)
 
     def go_to_wishlist(self):
         from src.pages.wishlist_page import WishlistPage
         logger.info("click wishlist")
         self.page.get_by_role("link", name="Wishlist").click()
-        return CartPage(self.page)
+        return WishlistPage(self.page)
 
     
     def go_to_login(self):
         from src.pages.login_page import LoginPage
         logger.info("click Login")
-        self.my_account.click()
+        self.page.get_by_role("button", name=" My account ").click()
         self.page.get_by_role("link", name=" Login").first.click() 
         return LoginPage(self.page)
 
     def go_to_register(self):
         from src.pages.register_page import RegisterPage
         logger.info("click Register")
-        self.my_account.click()
+        self.page.get_by_role("button", name=" My account ").click()
         self.page.get_by_role("link", name=" Register").first.click()
         return RegisterPage(self.page)
     
